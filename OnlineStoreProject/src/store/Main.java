37852e17b81b00ecb55f7f828d2427d2d22081d2
imran <imranbabur111@gmail.com>
2019-05-01 14:43:00 -0400

    first commit

---
+++ OnlineStoreProject/src/store/Main.java
@@ -0,0 +1,18 @@
+package store;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	public static long square(int x) {
+		long y = x * (long) x;
+		x = -1;
+		return y;
+		}
+	public static void main(String[] args) {
+		int value = 9;
+		long result = square(value);
+		System.out.println(value);
+		System.out.println(result);
+	} 
+}
